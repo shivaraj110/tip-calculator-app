f72702b42d1073d4f973eb96ea0e480f130edfa3
shivaraj-kc <shivarajchandaragi9@gmail.com>
2024-08-31 22:06:12 +0530

    fixed tip render issue

---
+++ frontend/src/atoms/billAtoms.ts
@@ -0,0 +1,13 @@
+import { atom } from "recoil";
+export const billAtom = atom({
+    key : 'billAtom',
+    default : 0
+})
+export const poepleAtom = atom({
+    key : 'peopleAtom',
+    default : 0
+})
+export const tipAtom = atom({
+    key : 'tipAtom',
+    default:0
+})
---
+++ frontend/src/components/PercentageBtn.tsx
@@ -0,0 +1,16 @@
+import { useRecoilState, useRecoilValue } from "recoil";
+import { billAtom, poepleAtom, tipAtom } from "../atoms/billAtoms";
+export const PercentageBtn = ({ label , p }: any) => {
+  const bill = useRecoilValue(billAtom);
+  const People = useRecoilValue(poepleAtom);
+  const [tip, setTip] = useRecoilState(tipAtom);
+  return (
+    <button
+      className="bg-btn-cyan rounded-md hover:bg-amt-cyan py-2 text-center w-full"
+      onClick={() => {
+        setTip(bill * p * People);
+      }}>
+      {label}
+    </button>
+  );
+};
